aeba43fa3c72cb3926ac5c2c1254084dd65fe7c3
arijit203 <arijit.saha1373@gmail.com>
2024-07-01 16:26:52 +0530

    Implemented Authentication Features

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import ToasterContext from "./context/ToasterContext";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Messenger Clone",
+  description: "Messenger Clone",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children:React.ReactNode
+} ){
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ToasterContext/>
+        {children}
+        
+      </body>
+    </html>
+  );
+}
