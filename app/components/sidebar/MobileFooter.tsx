cb4de379f428796f9ea00ec562489000c26f4cbc
arijit203 <arijit.saha1373@gmail.com>
2024-07-07 21:34:23 +0530

    Changes for vercel

--- app/components/sidebar/MobileFooter.tsx
+++ app/components/sidebar/MobileFooter.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import React, { useState } from 'react'
-import useRoutes from '../../hooks/useRoutes'
-import useConversation from '../../hooks/useConversation'
+import React, { useState } from 'react';
+import useRoutes from '../../hooks/useRoutes';
+import useConversation from '../../hooks/useConversation';
 import MobileItem from './MobileItem';
 import MobileLink from './MobileLink';
 import Avatar from '../Avatar';
@@ -11,48 +11,47 @@ import SettingsModal from './SettingsModal';
 
 function MobileFooter({ currentUser }:{currentUser:User}) {
 
-    const routes=useRoutes();
-    const {isOpen}=useConversation();
-    if(isOpen) return null;
+    const routes = useRoutes();
+    const { isOpen } = useConversation();
+    const [open, setOpen] = useState(false);
 
-    const [open,setOpen]=useState(false);
+    if (isOpen) return null;
 
-
-  return (
-    <>
-    <SettingsModal currentUser={currentUser} isOpen={open} onClose={()=>setOpen(false)}/>
-    <div className='
-    fixed
-    justify-between
-    w-full
-    bottom-0
-    z-40
-    flex
-    items-center
-    bg-white
-    border-t-[1px]
-    lg:hidden'
-  >
-    {
-                routes.map((item)=>(
-                    <MobileItem
-                    key={item.label}
-                    href={item.href}
-                    label={item.label}
-                    icon={item.icon}
-                    active={item.active}
-                    onClick={item.onClick}/>
-                ))
-               }
-       <MobileLink>
-       <div onClick={() => setOpen(true)} className="cursor-pointer hover:opacity-75 transition">
-            
-            <Avatar user={currentUser}/>
-          </div>
-        </MobileLink>
-  </div>
-  </>
-  )
+    return (
+        <>
+            <SettingsModal currentUser={currentUser} isOpen={open} onClose={() => setOpen(false)} />
+            <div className='
+                fixed
+                justify-between
+                w-full
+                bottom-0
+                z-40
+                flex
+                items-center
+                bg-white
+                border-t-[1px]
+                lg:hidden'
+            >
+                {
+                    routes.map((item) => (
+                        <MobileItem
+                            key={item.label}
+                            href={item.href}
+                            label={item.label}
+                            icon={item.icon}
+                            active={item.active}
+                            onClick={item.onClick}
+                        />
+                    ))
+                }
+                <MobileLink>
+                    <div onClick={() => setOpen(true)} className="cursor-pointer hover:opacity-75 transition">
+                        <Avatar user={currentUser} />
+                    </div>
+                </MobileLink>
+            </div>
+        </>
+    );
 }
 
-export default MobileFooter
+export default MobileFooter;
